e3eb24bc70b5a2e855681ecfd59e919aceaba7d5
Chromolous <alexmachadofm@gmail.com>
2026-04-18 22:36:25 -0300

    Finalizando as mensagens

--- src/components/Teste.vue
+++ src/components/Teste.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
-import { computed, ref } from 'vue'
+import { ref } from 'vue'
 import yipe from '../assets/yipee.m4a'
+import boom from '../assets/boom.m4a'
 
 const isOpen = ref(false)
 const temConfete = ref(false)
@@ -12,6 +13,7 @@ function openCurtain() {
   isOpen.value = true
   temConfete.value = true
   new Audio(yipe).play();
+
 }
 
 function configConfete() {
@@ -29,6 +31,7 @@ let contadorPresente = 0
 let scale = ref(1)
 function abrePresente() {
     if (contadorPresente >= 7) {
+        new Audio(boom).play();
         openCurtain()
     } else {
         contadorPresente++
@@ -39,14 +42,62 @@ function abrePresente() {
 
 function prox_pagina() {
     page.value++
+    if (page.value == 3) {
+        new Audio(boom).play();
+    }
+    if (page.value == 10) {
+        new Audio(yipe).play();
+
+    }
 }
 
-const mensagem1 = `
-Oi Carla, quanto tempo né? Se pa você ta meio perdida, mas vai fazer sentido (eu acho).\n
-Primeiramente, parabéns pelo o seu dia! 17 anos? Me perdi na conta.\n
-A gente era criança quando nos conhecemos e agora eu to me sentindo um velho. "Ai mas você sempre foi meio idoso" xiu, não ligo`
+const mensagem1 = `Oi Carla, quanto tempo né? Se pa você ta meio perdida, mas vai fazer sentido (eu acho).
+Primeiramente, parabéns pelo o seu dia! 17 anos? Me perdi na conta.
+A gente era criança quando nos conhecemos e agora eu to me sentindo um velho.
+"Ai mas você sempre foi meio idoso" fala isso na minha cara, covarde.`.split('')
 
-const msg1Array = computed(() => mensagem1.split(''))
+const mensagem2 = `Bom, nós paramos de nos falar de uma forma... inadequada eu diria. 
+Então eu queria acertar os assuntos com você :)
+Sendo assim...
+`.split('')
+
+const mensagem3 = `EU NÃO DISSE NADA!!!`
+
+const mensagem4 = `Quando eu tomei block eu fui procurar saber o que tinha acontecido, e me falaram que a praga do Davi espalhou
+MENTIRAS
+DESINFORMAÇÃO
+CALÚNIAS 
+sobre coisas que eu falei ou deixei de falar, e imaginei que esse tivesse sido o motivo para acontecer o que aconteceu.
+`
+
+const mensagem5 = `De fato, na época nós estávamos criando uma distancia aos poucos por... vários motivos.
+Mas nada que me fizesse desgostar de você ou falar mal de você!
+`
+
+const mensagem6 = `Enfim, nem sei se esse foi realmente o estopim para a sua decisão, mas agora já passou tanto tempo que tanto faz.
+E já que a gente parou de se falar, eu queria pelo menos fazer isso de um jeito menos zoado.
+`
+
+const mensagem7 = `É fato que ambos já participamos consideravelmente da vida um do outro, seja por bem ou por mal,
+e é por isso que eu gostaria que tivessemos boas memórias um do outro.
+Pensando nisso eu...
+bom
+er
+meio que tipo assim
+eu ia fazer um mega compilado de coisas nossas (fotos, vídeos, mensagens e etc)
+mas como eu sou um procrastinador top 10 eu acabei deixando pra depois e esquecendo.
+ops.
+`
+
+const mensagem8 = `MAS DE QUALQUER FORMA
+Sinto muito por todos os momentos ruins e queria te agradecer por todos os momentos bons que você proporcionou.
+Você é uma menina muito dedicada, talentosa, gentil e eu até poderia ficar citando todas as suas qualidades,
+mas aí você teria que ficar clicando nesse botão de continuar por um tempinho.
+`
+
+const mensagem9 = `Acho que é mais ou menos isso, essa provavelmente é a minha última mensagem pra você (ou pelo menos uma das últimas)
+Tchau Cah! Fica bem, espero todo sucesso do mundo pra você!
+`
 
 </script>
 
@@ -59,45 +110,113 @@ const msg1Array = computed(() => mensagem1.split(''))
 
     <div class="relative z-1 flex flex-col items-center justify-center h-full">
         <h1 v-if="page == 0" class="text-neutral-200 z-10">FELIZ ANIVERSÁRIO</h1>
-        <h1 v-if="page == 1" class="text-neutral-200 z-10">
-            <span
-                v-for="(char, index) in 'Mas antes, uma mensagem especial'.split('')"
-                :key="index"
-                class="inline-block opacity-0 animate-fadeIn"
-                :style="{ animationDelay: index * 0.05 + 's' }">
-                <template v-if="char === '\n'">
-                    <br>
-                </template>
-                <template v-else>
-                    {{ char === ' ' ? '\u00A0' : char }}
-                </template>
-            </span>
-        </h1>
-        <div v-if="page == 2" class="bg-black/20 w-1/2 h-fit rounded-lg py-2 px-2 mb-5">
+
+        <div v-if="page == 1" class="bg-black/20 w-16/20 h-fit rounded-lg py-2 px-2 mb-5">
             <p class="text-neutral-200">
-                <span
-                    v-for="(char, index) in msg1Array"
-                    :key="index"
-                    class="inline-block opacity-0 animate-fadeIn"
-                    :style="{ animationDelay: index * 0.03 + 's' }">
-                    <template v-if="char === '\n'">
-                        <br>
-                    </template>
-                    <template v-else>
+                <template v-for="(char, index) in mensagem1" :key="index">
+                    <br v-if="char === '\n'" />
+                    <span v-else class="inline-block opacity-0 animate-fadeIn" :style="{ animationDelay: index * 0.01 + 's' }">
                         {{ char === ' ' ? '\u00A0' : char }}
-                    </template>
-                </span>
+                    </span>
+                </template>
             </p>
         </div>
+
+        <div v-if="page == 2" class="bg-black/20 w-16/20 h-fit rounded-lg py-2 px-2 mb-5">
+            <p class="text-neutral-200">
+                <template v-for="(char, index) in mensagem2" :key="index">
+                    <br v-if="char === '\n'" />
+                    <span v-else class="inline-block opacity-0 animate-fadeIn" :style="{ animationDelay: index * 0.01 + 's' }">
+                        {{ char === ' ' ? '\u00A0' : char }}
+                    </span>
+                </template>
+            </p>
+        </div>
+
+        <h1 v-if="page == 3" class="text-neutral-200 font-bold">
+            {{mensagem3}}
+        </h1>
+
+        <div v-if="page == 4" class="bg-black/20 w-16/20 h-fit rounded-lg py-2 px-2 mb-5">
+            <p class="text-neutral-200">
+                <template v-for="(char, index) in mensagem4" :key="index">
+                    <br v-if="char === '\n'" />
+                    <span v-else class="inline-block opacity-0 animate-fadeIn" :style="{ animationDelay: index * 0.01 + 's' }">
+                        {{ char === ' ' ? '\u00A0' : char }}
+                    </span>
+                </template>
+            </p>
+        </div>
+
+        <div v-if="page == 5" class="bg-black/20 w-16/20 h-fit rounded-lg py-2 px-2 mb-5">
+            <p class="text-neutral-200">
+                <template v-for="(char, index) in mensagem5" :key="index">
+                    <br v-if="char === '\n'" />
+                    <span v-else class="inline-block opacity-0 animate-fadeIn" :style="{ animationDelay: index * 0.01 + 's' }">
+                        {{ char === ' ' ? '\u00A0' : char }}
+                    </span>
+                </template>
+            </p>
+        </div>
+
+        <div v-if="page == 6" class="bg-black/20 w-16/20 h-fit rounded-lg py-2 px-2 mb-5">
+            <p class="text-neutral-200">
+                <template v-for="(char, index) in mensagem6" :key="index">
+                    <br v-if="char === '\n'" />
+                    <span v-else class="inline-block opacity-0 animate-fadeIn" :style="{ animationDelay: index * 0.01 + 's' }">
+                        {{ char === ' ' ? '\u00A0' : char }}
+                    </span>
+                </template>
+            </p>
+        </div>
+
+        <div v-if="page == 7" class="bg-black/20 w-16/20 h-fit rounded-lg py-2 px-2 mb-5">
+            <p class="text-neutral-200">
+                <template v-for="(char, index) in mensagem7" :key="index">
+                    <br v-if="char === '\n'" />
+                    <span v-else class="inline-block opacity-0 animate-fadeIn" :style="{ animationDelay: index * 0.01 + 's' }">
+                        {{ char === ' ' ? '\u00A0' : char }}
+                    </span>
+                </template>
+            </p>
+        </div>
+
+        <div v-if="page == 8" class="bg-black/20 w-16/20 h-fit rounded-lg py-2 px-2 mb-5">
+            <p class="text-neutral-200">
+                <template v-for="(char, index) in mensagem8" :key="index">
+                    <br v-if="char === '\n'" />
+                    <span v-else class="inline-block opacity-0 animate-fadeIn" :style="{ animationDelay: index * 0.01 + 's' }">
+                        {{ char === ' ' ? '\u00A0' : char }}
+                    </span>
+                </template>
+            </p>
+        </div>
+
+        <div v-if="page == 9" class="bg-black/20 w-16/20 h-fit rounded-lg py-2 px-2 mb-5">
+            <p class="text-neutral-200">
+                <template v-for="(char, index) in mensagem9" :key="index">
+                    <br v-if="char === '\n'" />
+                    <span v-else class="inline-block opacity-0 animate-fadeIn" :style="{ animationDelay: index * 0.01 + 's' }">
+                        {{ char === ' ' ? '\u00A0' : char }}
+                    </span>
+                </template>
+            </p>
+        </div>
+
+        <h1 v-if="page == 10" class="text-neutral-200 font-bold">
+            <span class="inline-block opacity-0 animate-fadeIn" :style="{ animationDelay: 0.1 + 's' }">
+                Feito com ❤ por "Jossoares" (ou "jossoaris?")
+            </span>
+        </h1>
         
-        <button @click="prox_pagina()" class="bg-white opacity-85 rounded-xl px-6 py-3 shadow-lg hover:cursor-pointer active:scale-90 transition duration-100">
+        <button v-if="page < 10" @click="prox_pagina()" class="bg-white opacity-85 rounded-xl px-6 py-3 shadow-lg hover:cursor-pointer active:scale-90 transition duration-100">
             Continuar...
         </button>
     </div>
 
     <!--E tome confete-->
 
-    <div v-if="page < 2" class="absolute inset-0 pointer-events-none z-10">
+    <div v-if="page < 1 || page == 10" class="absolute inset-0 pointer-events-none z-10">
         <div
             v-for="i in 40"
             :key="i"
